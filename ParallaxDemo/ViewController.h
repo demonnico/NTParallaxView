951c16f35d92be7ebb58669dbde5408faa3aa5ac
Nicholas Tau <demon42111915@gmail.com>
2013-09-12 23:03:18 +0800

    Initial Commit

---
+++ ParallaxDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ParallaxDemo
+//
+//  Created by demon on 9/12/13.
+//  Copyright (c) 2013 demonNico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ParallaxDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ParallaxDemo
+//
+//  Created by demon on 9/12/13.
+//  Copyright (c) 2013 demonNico. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
