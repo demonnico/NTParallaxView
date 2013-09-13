9cc74c080db8d51cfc7b9e9ed8972b36c2218e67
Nicholas Tau <demon42111915@gmail.com>
2013-09-13 11:30:25 +0800

    set UINavigationController as root View Controller

--- ParallaxDemo/AppDelegate.m
+++ ParallaxDemo/AppDelegate.m
@@ -17,7 +17,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
+    self.window.rootViewController =
+    [[UINavigationController alloc] initWithRootViewController:self.viewController];
     [self.window makeKeyAndVisible];
     return YES;
 }
